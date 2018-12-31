05c48741c8a9816db9fa50d492aebbd3d57eb41a
Alvin Tan <alvintan626@berkeley.edu>
2018-12-30 20:43:52 -0800

    Update stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,32 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  
+  if(typeof obj === "string"){return '"' + obj + '"';}
+  if(typeof obj === "undefined"){return "";}
+  if(obj === null){return "null";}
+
+  if(Array.isArray(obj)){
+    var arr = [];
+
+    obj.forEach(function(element){
+      arr.push(stringifyJSON(element));
+    })
+    return "[" + arr.toString() + "]";
+  }
+
+  if(typeof obj === "object"){
+    var stringObj = "";
+    var objKeys = Object.keys(obj);
+    objKeys.forEach(function(k){
+      if(typeof k === "function" || typeof k === "undefined" || typeof obj[k] === "function" || typeof obj[k] === "undefined"){
+        return "";
+      }
+      else{stringObj += stringifyJSON(k) + ':' + stringifyJSON(obj[k]) + ',';
+      }
+    })
+    return "{" + stringObj.slice(0, stringObj.length - 1) + "}"
+    }
+
+  return obj.toString();
 };
